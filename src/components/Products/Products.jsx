c013edfef071f0c6562cbf4c1bd16f01a1604fea
AndyLinxies <linxies@gmail.com>
2021-07-31 17:58:18 +0200

    seach

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,7 +4,9 @@ import   {Grid}  from '@material-ui/core';
 
 //import du component Product
 import Product from './Product/Product'; 
-
+//
+import {useState} from 'react'
+//
 import useStyles from './styles';
 //les images
 // import pc from '../../img/windows.jpg';
@@ -33,6 +35,8 @@ import chaussure from '../../img/shoes1.jpg';
 
 const Products = ({products, onAjout}) => {
     const classes = useStyles();
+    //recherche
+    const [searchTerm,setSearchTerm]= useState('');
 // console.log(products);
     return (
         <main className={classes.content}>
@@ -40,8 +44,17 @@ const Products = ({products, onAjout}) => {
         <div className={classes.toolbar}/>
         {/* Component venant de material-ui */}
         {/* xs=12 pour qu'il prennes tout l'espace dispo sur mobile, sm et lg pour tablette et grand ecran */ }
+        <div className='search'>
+            <input className='myInput' type="text" placeholder="Search..." onChange={event=>{setSearchTerm(event.target.value)}} />
+        </div>
             <Grid container justifyContent="center" spacing={4}>
-                {products.map((product) => (
+                {products.filter((product)=>{
+                    if(searchTerm==''){
+                        return product
+                    }else if(product.name.toLowerCase().includes(searchTerm.toLocaleLowerCase())){
+                        return product
+                    }
+                }).map((product) => (
                     <Grid item key={product.id}  xs={12} sm={6} md={4} lg={3}>
                     {/* Le component Product sans S est appelé et sa props product est l'elem de la boucle*/ }
                         <Product product={product} onAjout={onAjout}/>
